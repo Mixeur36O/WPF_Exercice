53e9e58202e51250142fc4c1baf6e3e9349ee6e6
Limet Maxence <200040@CCM-Asty-Moulin.local>
2025-10-14 14:22:15 +0200

    14/10/2025

--- Event1/6TTI_Limet_Maxence_Event1/MainWindow.xaml.cs
+++ Event1/6TTI_Limet_Maxence_Event1/MainWindow.xaml.cs
@@ -19,6 +19,36 @@ namespace _6TTI_Limet_Maxence_Event1
         public MainWindow()
         {
             InitializeComponent();
+            btnCalculer.Click += new RoutedEventHandler(BtnCalculer_Click);
+        }
+
+        private void BtnCalculer_Click(object sender, RoutedEventArgs e)
+        {
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            string message = "";
+            ResoudTrinome(a,b,c,out message);    
+        }
+        private void ResoudTrinome(double a, double b, double c, out string message)
+        {
+            double delta = Math.Pow(b, 2) - 4 * a * c;
+            if (delta < 0)
+            {
+                message = "Il n'y a pas de solution réelle";
+
+            }
+            else if (delta == 0)
+            {
+                double x1 = -b / (2 * a);
+                message = "Il y a une solution " + x1;
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                message = "Il y a deux solutions " + x1 + " et " + x2;
+            }
         }
     }
 }
